0a1ed313664adaef868440e239df5b97fd8d5a88
kang <wang_xingkang@qq.com>
2020-04-07 10:42:59 +0800

    fix: 关闭报错的 eslint 规则

--- packages/plugin-eslint/src/config/eslint.react-typescript.config.js
+++ packages/plugin-eslint/src/config/eslint.react-typescript.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  extends: [
-    'alloy',
-    'alloy/react',
-    'alloy/typescript',
-  ]
+  extends: ['alloy', 'alloy/react', 'alloy/typescript'],
+  rules: {
+    // ts 中用 a?.b?.method()提示no-unused-expressions错误
+    'no-unused-expressions': 'off'
+  }
 };
