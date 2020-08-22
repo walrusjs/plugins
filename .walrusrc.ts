4947c6dd0ef195031670dd2633d692e1ad075539
wangxingkang <wang_xingkang@qq.com>
2020-08-22 19:56:31 +0800

    chore: 关闭同步淘宝源

--- .walrusrc.ts
+++ .walrusrc.ts
@@ -2,10 +2,7 @@ import { Config } from '@walrus/types';
 
 const config: Config = {
   plugins: ['./packages/plugin-release/lib/index.js'],
-  presets: ['./packages/preset-lint/lib/index.js'],
-  release: {
-    skipSync: false
-  }
+  presets: ['./packages/preset-lint/lib/index.js']
 };
 
 export default config;
