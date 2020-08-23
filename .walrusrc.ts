936d95e1f4c0ee40fd55143e6f7d45e2db56c991
wangxingkang <wang_xingkang@qq.com>
2020-08-23 13:19:20 +0800

    feat: plugin init

--- .walrusrc.ts
+++ .walrusrc.ts
@@ -3,6 +3,7 @@ import { Config } from '@walrus/types';
 const config: Config = {
   plugins: [
     './packages/plugin-test/lib/index.js',
+    './packages/plugin-build/lib/index.js',
     './packages/plugin-release/lib/index.js'
   ],
   presets: ['./packages/preset-lint/lib/index.js']
